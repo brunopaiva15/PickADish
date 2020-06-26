c005a17ba80edc8814652e1232bb0ffb07a754ba
Bruno Paiva <ppaiva11144@gmail.com>
2020-06-26 16:16:52 +0200

    Bye bye CocoaPods

--- Pods/Target Support Files/AUPickerCell/AUPickerCell-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AUPickerCell : NSObject
-@end
-@implementation PodsDummy_AUPickerCell
-@end
--- Pods/Target Support Files/Pods-PickADish/Pods-PickADish-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PickADish : NSObject
-@end
-@implementation PodsDummy_Pods_PickADish
-@end
